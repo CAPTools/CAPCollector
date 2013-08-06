70b060ac330a1b7903b6db455c4237e6128d3123
Art Botterell <acb@incident.com>
2013-08-06 13:46:12 -0700

    Initial commits

--- cgi-bin/cap_support.py
+++ cgi-bin/cap_support.py
@@ -1,33 +1,11 @@
-#     cap_support.py -- python support for CAPCollector (post_cap)
-#     version 0.9 - 31 July 2013
+#     cap_support.py -- python support objects for CAPCollector (post_cap.py)
+#     version 0.9 - 6 August 2013
 #     
 #     Copyright (c) 2013, Carnegie Mellon University
 #     All rights reserved.
 # 
-#     Redistribution and use in source and binary forms, with or without modification, are permitted 
-#     provided that the following conditions are met:
-#     
-#     * Redistributions of source code must retain the above copyright notice, this list of conditions
-#     and the following disclaimer.
-#     
-#     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions 
-#     and the following disclaimer in the documentation and/or other materials provided with the distribution.
-#     
-#     * Neither the name of Carnegie Mellon University nor the names of its contributors may be used to endorse or 
-#     promote products derived from this software without specific prior written permission.
-#     
-#     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
-#     IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
-#     FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS 
-#     BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-#     BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
-#     BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
-#     LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
-#     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# 
-#     Contributors:  Art Botterell <art.botterell@sv.cmu.edu>, <acb@incident.com>, <art@raydant.com>
+#    See LICENSE.txt for license terms (Modified BSD) 
 #
-#     Dependencies:  requires the python-dateutil and pytz packages
 #     
 
 from datetime import datetime, timedelta
